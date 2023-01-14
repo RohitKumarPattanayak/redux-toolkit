c52407288b8dcea3f4457981c02a824434b79f3f
rohitkp <rohit.p@spurtreetech.com>
2023-01-14 19:47:45 +0530

    completed basic redux toolkit app

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,40 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import React from "react";
+import { increase, decrease, reset } from "./counterSlice";
+import styles from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Counter() {
-  const count = useSelector(selectCount);
+  const stateCount = useSelector((state) => state.counter.count);
+  let [addValue, setAddValue] = React.useState(0);
+  function onChangeHandle(e) {
+    setAddValue(e.target.value);
+  }
+  function onResetHandle(e) {
+    dispatch(reset());
+    setAddValue(0);
+  }
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
-  const incrementValue = Number(incrementAmount) || 0;
-
   return (
     <div>
       <div className={styles.row}>
         <button
           className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
+          onClick={() => dispatch(increase({ Value: addValue }))}
         >
           +
         </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
+        <span className={styles.value}>{stateCount}</span>
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
+          onClick={() => dispatch(decrease({ Value: addValue }))}
         >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
+          -
         </button>
       </div>
+      <div>
+        <input name="addValue" value={addValue} onChange={onChangeHandle} />
+      </div>
+      <button onClick={onResetHandle}>reset</button>
     </div>
   );
 }
